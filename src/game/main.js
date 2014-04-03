8fcb4982d74873d67b908dd6df583c84cd2b3d7b
Michal Budzynski <michal@virtualdesign.pl>
2014-04-04 05:50:28 +0700

    [YL] all the animations visible at once

--- src/game/main.js
+++ src/game/main.js
@@ -17,9 +17,21 @@ var state = {
     },
     create: function(){
       // State create logic goes here
-      var character = game.add.sprite(300, 200, 'character');
-      character.animations.add('walk');
+      var character = game.add.sprite(0, 0, 'character');
+      character.animations.add('walk', [0, 1, 2, 3]);
       character.animations.play('walk', 6, true);
+
+      var character2 = game.add.sprite(190, 0, 'character');
+      character2.animations.add('walk', [4, 5, 6, 7]);
+      character2.animations.play('walk', 6, true);
+
+      var character3 = game.add.sprite(0, 190, 'character');
+      character3.animations.add('walk', [8, 9, 10, 11]);
+      character3.animations.play('walk', 6, true);
+
+      var character4 = game.add.sprite(190, 190, 'character');
+      character4.animations.add('walk', [12, 13, 14, 15]);
+      character4.animations.play('walk', 6, true);
     },
     update: function() {
         // State Update Logic goes here.
