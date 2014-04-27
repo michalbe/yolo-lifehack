d5ff5287bfd3dc1504b0fdfa134a863875c6a015
Michal Budzynski <michal@virtualdesign.pl>
2014-04-27 21:47:56 +0200

    [YOLO-LIFEHACK] - Last ipp commit

--- src/lib/game/main.js
+++ src/lib/game/main.js
@@ -6,6 +6,18 @@ ig.module(
 	'game.levels.level1'
 )
 .defines(function() {
+
+	ig.EntityExtended.inject({
+    update: function() {
+      //if (this.pos.y < this.tileSize.y*4 && !this.isUiScreen) {
+      //    this.zIndex = 10100;
+      //} else {
+      this.zIndex = this.pos.y + this.size.y;
+      //}
+      this.parent();
+    }
+	});
+
 	var myGameClass = ig.GameExtended.extend({
 		init: function () {
 			this.parent();
