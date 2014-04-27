0ab442f84b813a740f68139eb2ba64be36704641
Michal Budzynski <michal@virtualdesign.pl>
2014-04-27 22:31:31 +0200

    [YOLO-LIFEHACK] - Collisions fixed

--- src/lib/game/entities/building.js
+++ src/lib/game/entities/building.js
@@ -5,13 +5,12 @@ ig.module(
 ).defines( function() {
   ig.global.EntityBuilding = ig.EntityExtended.extend({
     size: {x: 360, y: 180},
-    offset: {x: -10, y: 144},
+    offset: {x: 0, y: 144},
     animSheet: new ig.AnimationSheet( "media/tiles/building1.png", 360, 324),
     animInit: "idleX",
     animSettings: {
       idleX: { sequence: [0], frameTime: 0.1 },
-    },
-    collides: ig.EntityExtended.COLLIDES.FIXED
+    }
   });
 
 });
