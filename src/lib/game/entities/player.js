e1b4db9d7f1b0b559c6389bd7de75a2076b35d1d
Michal Budzynski <michal@virtualdesign.pl>
2014-06-01 03:35:53 +0200

    impact++ to impact & camera change

--- src/lib/game/entities/player.js
+++ src/lib/game/entities/player.js
@@ -1,30 +1,78 @@
 ig.module(
-  'game.entities.player'
-).requires(
-  'plusplus.abstractities.player'
-).defines( function() {
-  ig.EntityPlayer = ig.global.EntityPlayer = ig.Player.extend({
-    canFlipY: false,
-    canFlipX: false,
-    size: {x: 125, y: 50},
-    offset: {x: 25, y: 130},
-    animSheet: new ig.AnimationSheet( "media/sprites/char-big.png", 180, 180),
-    animInit: "idleX",
-    speed: {
-      x: 500,
-      y: 500
-    },
-    maxVelGrounded: {
-        x: 500,
-        y: 500
-    },
-    animSettings: {
-      idleX: { sequence: [4], frameTime: 0.1 },
-      moveDown: { sequence: [4, 5, 6, 7], frameTime: 0.09 },
-      moveUp: { sequence: [12, 13, 14, 15], frameTime: 0.09 },
-      moveRight: { sequence: [0, 1, 2, 3], frameTime: 0.09 },
-      moveLeft: { sequence: [8, 9, 10, 11], frameTime: 0.09 }
+	'game.entities.player'
+)
+.requires(
+	'impact.entity'
+)
+.defines(function(){
+
+ig.global.EntityPlayer = ig.Entity.extend({
+
+	// The players (collision) size is a bit smaller than the animation
+	// frames, so we have to move the collision box a bit (offset)
+  size: {x: 125, y: 50},
+  offset: {x: 25, y: 130},
+
+	maxVel: {x: 400, y: 800},
+	friction: {x: 800, y: 0},
+
+  speed: 300,
+
+	type: ig.Entity.TYPE.A, // Player friendly group
+	checkAgainst: ig.Entity.TYPE.NONE,
+	collides: ig.Entity.COLLIDES.PASSIVE,
+
+	animSheet: new ig.AnimationSheet( "media/sprites/char-big.png", 180, 180),
+
+	// These are our own properties. They are not defined in the base
+	// ig.Entity class. We just use them internally for the Player
+	flip: false,
+	accelGround: 1200,
+
+
+	init: function( x, y, settings ) {
+		this.parent( x, y, settings );
+
+		// Add the animations
+		this.addAnim( 'idle', 1, [4], true );
+		this.addAnim( 'moveUp', 0.09, [12, 13, 14, 15] );
+		this.addAnim( 'moveDown', 0.09, [4, 5, 6, 7] );
+		this.addAnim( 'moveLeft', 0.09, [8, 9, 10, 11]); // stop at the last frame
+		this.addAnim( 'moveRight', 0.09, [0, 1, 2, 3] );
+
+		// Set a reference to the player on the game instance
+		ig.game.player = this;
+	},
+
+
+	update: function() {
+
+    if (ig.input.state('up')) {
+      this.vel.y = -this.speed;
+      this.vel.x = 0;
+      this.currentAnim = this.anims.moveUp;
+    } else if (ig.input.state('left')) {
+      this.vel.y = 0;
+      this.vel.x = -this.speed;
+      this.currentAnim = this.anims.moveLeft;
+    } else if (ig.input.state('right')) {
+      this.vel.y = 0;
+      this.vel.x = this.speed;
+      this.currentAnim = this.anims.moveRight;
+    } else if (ig.input.state('down')) {
+      this.vel.y = this.speed;
+      this.vel.x = 0;
+      this.currentAnim = this.anims.moveDown;
+    } else {
+      this.vel.y = 0;
+      this.vel.x = 0;
+      this.currentAnim = this.anims.idle;
     }
-  });
+
+		// Move!
+		this.parent();
+	}
+});
+
 
 });
