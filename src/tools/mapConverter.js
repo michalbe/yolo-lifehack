b211c92c04afa842483183e8946f452a43cc0164
Michal Budzynski <michal@virtualdesign.pl>
2014-06-01 12:10:25 +0700

    [YOLO-LIFEHACK] - save level1.js file in proper location

--- src/tools/mapConverter.js
+++ src/tools/mapConverter.js
@@ -2,8 +2,9 @@ var fs = require('fs');
 var mapSchema = process.argv[2];
 
 var tileSize = 180;
-var currentX = 0;
-var currentY = 0;
+var currentX = -tileSize;
+var currentY = -tileSize;
+
 var header = "ig.module('game.levels.level1').requires('impact.image','game.entities.wall','game.entities.player').defines(function(){LevelLevel1=/*JSON[*/{\"entities\":[";
 var footer = "],\"layer\":[{\"name\":\"floor\",\"width\":15,\"height\":15,\"linkWithCollision\":false,\"visible\":1,\"tilesetName\":\"media/tiles/floors.jpg\",\"repeat\":false,\"preRender\":false,\"distance\":\"1\",\"tilesize\":180,\"foreground\":false,\"data\":[[7,7,7,7,7,2,2,2,2,2,2,1,1,1,1],[7,7,7,7,7,2,2,2,2,2,2,1,1,1,1],[7,7,7,7,7,2,2,2,2,2,2,1,1,1,1],[2,2,2,2,2,2,2,6,6,6,6,1,1,1,1],[2,2,2,2,2,2,2,6,6,6,6,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]}]}/*]JSON*/;LevelLevel1Resources=[new ig.Image('media/tiles/floors.jpg')];});";
 var emptyTile = 9;
@@ -22,11 +23,11 @@ fs.readFile('./' + mapSchema, function read(err, data) {
 var createMapFromScheme = function(scheme) {
   rowNumbers = scheme.length;
   scheme.forEach(resolveRow);
-  fs.writeFile('level.js', header + entities.join(',') + footer, function(err){
+  fs.writeFile('src/lib/game/levels/level1.js', header + entities.join(',') + footer, function(err){
     if (err) {
-      console.log('Could not update level.js file');
+      console.log('Could not update level1.js file');
     } else {
-      console.log('Level saved in level.js');
+      console.log('Level saved in level1.js');
     }
   });
 }
