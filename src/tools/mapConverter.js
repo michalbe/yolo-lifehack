a4f42d7018467988d7b7afebf6d80845e2dd4ee6
Michal Budzynski <michal@virtualdesign.pl>
2014-06-01 11:18:53 +0700

    [YOLO-LIFEHACK] - config variables

--- src/tools/mapConverter.js
+++ src/tools/mapConverter.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var mapSchema = process.argv[2];
 
+var tileSize = 180;
+var currentX = 0;
+var currentY = 0;
+
 fs.readFile('./' + mapSchema, function read(err, data) {
   if (err) {
       throw err;
@@ -10,5 +14,5 @@ fs.readFile('./' + mapSchema, function read(err, data) {
 
 
 var createMapFromScheme = function(scheme) {
-  
+
 }
